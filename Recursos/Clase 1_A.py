3f6d0b94ee0129e4902e60bd789e930ca0ecbafd
vpchimboc <32884862+vpchimboc@users.noreply.github.com>
2022-10-15 19:51:20 -0500

    v1
    V1

---
+++ Recursos/Clase 1_A.py
@@ -0,0 +1,59 @@
+#1. Declaración de variables
+#Python puede identificar el tipo de dato que quiere almacenar
+
+#Python puede diferenciar mayúsculas de minúsculas
+
+#Ejercicio 1
+"""2. Manipulación de cadenas de caracteres
+Operaciones
+"""
+
+"""3. Palabras reservadas
+Las palabras reservadas, son identificadores para uso exclusivo del lenguaje de programaci[on, que no pueden ser utilizadas para identificar y nombrar variables, metodos, objetos o cualquier elemento dentro de nuestro codigo
+Existen 28 palabras reservadas
+and     del     for         is      raise   assert
+if      else    elif        from    lambda  return
+break   global  not         try     class   except
+or      while   continue    exec    import  yield
+def     finally in          print
+"""
+
+"""4. Operadores aritméticos
+Son aquellos que manipulan datos numéricos, tanto numéricos enteros, asi como decimales también conocidos como reales
+suma (+)
+rest (-)
+multipliciones(*)
+divisiones(/)
+modulo o residuo(%) 
+exponenciales(**)
+División entera(//)
+"""
+
+"""5. Comentarios 
+Son anotaciones legibles del programador en el código fuente del programa.
+En Python, la forma de añadir comentarios son los siguientes
+- Con el signo # para linea de código
+- comillas dobless sin asignación""
+- comillas triples """ """
+"""
+
+"""6. Tipos de Datos en Python
+1. Enteros y Largos -: int y long
+2. Flotantes-: float
+3. Números complejos-: complex
+4. Cadenas-: str
+5. Booleanos-: bool
+"""
+
+"""7. Entrada de datos desde teclado
+Permite que el usuario pueda interactuar con el programa
+"""
+
+"""8. Sentencias Condicionales
+Es una instrucción o grupo de instrucciones, que se ejecuta cuando a un programa se le establece una condición lógica.
+Al cumplirse dicha condición, el programa ejecuta la instrucción que ha sido asignada a esta condición
+Sentencias condicionales simples
+if Condicion lógica:
+   Instrucción
+
+"""
