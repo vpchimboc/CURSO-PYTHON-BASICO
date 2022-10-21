6c1024bd1655eb90d2e2295bd792500544dec908
vpchimboc <32884862+vpchimboc@users.noreply.github.com>
2022-10-21 10:30:01 -0500

    errores

--- Clases/Clase4. Funciones/4. ErroresExep.py
+++ Clases/Clase4. Funciones/4. ErroresExep.py
@@ -1,4 +1,14 @@
 """
+Los principales excepciones definidas en Python son:
+
+TypeError : Ocurre cuando se aplica una operación o función a un dato del tipo inapropiado.
+ZeroDivisionError : Ocurre cuando se itenta dividir por cero.
+OverflowError : Ocurre cuando un cálculo excede el límite para un tipo de dato numérico.
+IndexError : Ocurre cuando se intenta acceder a una secuencia con un índice que no existe.
+KeyError : Ocurre cuando se intenta acceder a un diccionario con una clave que no existe.
+FileNotFoundError : Ocurre cuando se intenta acceder a un fichero que no existe en la ruta indicada.
+ImportError : Ocurre cuando falla la importación de un módulo.
+
 try:
 	# Codigo a ejecutar
 	# Pero podria haber errores en este bloque
